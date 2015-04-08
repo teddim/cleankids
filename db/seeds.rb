ede1611c4672cbeeff8e5c1972173760c286d59b
Teddi Maull <teddi@picturetheday.com>
2015-04-08 13:10:00 -0600

    Add default tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tasks = Task.create([
+                    {description: 'Brush my teeth', task_type: 'daily routine'},
+                    {description: 'Get dressed' , task_type: 'daily routine'},
+                    {description: 'Wash my face' , task_type: 'daily routine'},
+                    {description: 'Brush my hair' , task_type: 'daily routine'},
+                    {description: 'Gather lunch and bookbag' , task_type: 'daily routine'},
+                    {description: 'Eat breakfast' , task_type: 'daily routine'},
+                    {description: 'Put shoes on' , task_type: 'daily routine'},
+                    {description: 'Make the bed' , task_type: 'daily routine'},
+                    {description: 'Take a shower' , task_type: 'daily routine'},
+                    {description: 'Take a bath' , task_type: 'daily routine'},
+                    {description: 'Eat dinner' , task_type: 'daily routine'},
+                    {description: 'Pick out clothes for tomorrow' , task_type: 'daily routine'},
+                    {description: 'Do a load of laundry' , task_type: 'chores'},
+                    {description: 'Fold laundry' , task_type: 'chores'},
+                    {description: 'Put away laundry' , task_type: 'chores'},
+                    {description: 'Load dishwasher' , task_type: 'chores'},
+                    {description: 'Empty dishwasher' , task_type: 'chores'},
+                    {description: 'Clear the kitchen table' , task_type: 'chores'},
+                    {description: 'Take the garbage out' , task_type: 'chores'},
+                    {description: 'Take recycle out' , task_type: 'chores'},
+                    {description: 'Pick up toys in the yard' , task_type: 'chores'},
+                    {description: 'Put away 15 things' , task_type: 'chores'},
+                    {description: 'Wash windows' , task_type: 'chores'},
+                    {description: 'Clean bathroom sink' , task_type: 'chores'},
+                    {description: 'Clean bathroom counter' , task_type: 'chores'},
+                    {description: 'Water plants' , task_type: 'chores'},
+                    {description: 'Vacuum a room' , task_type: 'chores'},
+                    {description: 'Clean your bedroom' , task_type: 'chores'}
+  ])
