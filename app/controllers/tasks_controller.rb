56731756d2f58ec8048da0b73cc67d1843b709f7
Teddi Maull <teddi@picturetheday.com>
2015-04-08 14:53:27 -0600

    Users can see a list of their tasks and community tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,10 @@ class TasksController < ApplicationController
 
   def index
 
+    @community_tasks = Task.where(user_id: nil)
+    @community_tasks = @community_tasks.order(:task_type)
+    @user_tasks = Task.where(user_id: 1)
+
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,30 @@ class Task < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :description
-  
+
+  def is_chore
+    if self.task_type.downcase == "chores"
+      true
+    else
+      false
+    end
+
+  end
+
+  def is_routine
+    if self.task_type.downcase == "daily routine"
+      true
+    else
+      false
+    end
+  end
+
+  def is_popular
+    if self.times_used == 4
+      true
+    else
+      false
+    end
+  end
+
 end
