1c282af44ca9378e122ed07273e8d44c168e8389
Teddi Maull <teddi@picturetheday.com>
2015-04-10 14:08:31 -0600

    Create a quick layout for the print page

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,11 +1,30 @@
 class ChartsController < ApplicationController
 
+  def index
+    @chart = Chart.first
+  end
+
   def new
 
   end
 
   def create
-    @chart = Chart.new
+    charts_tasks = ChartsTasks.find params[:chart][:task_ids]
+    @chart.tasks = charts_tasks
+    binding.pry
+    # @chart = Chart.new(chart_params)
+    # @chart.user_id = current_user.id
+    # if @chart.save
+    flash[:notice]='User data was updated'
+    redirect_to root_path
+  #  else
+    # redirect_to tasks_path
+  #  end
+    end
 
+  private
+
+  def chart_params
+    params[:chart][:task_ids] ||= []
   end
 end
