60a7ac6d34637e9209a9a65fc228a4d6e31ecc54
Teddi Maull <teddi@picturetheday.com>
2015-04-10 10:10:22 -0600

    Add actions for creating a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,12 +6,25 @@ class TasksController < ApplicationController
 
     @community_tasks = Task.where(user_id: nil)
     @community_tasks = @community_tasks.order(:task_type)
-    @user_tasks = Task.where(user_id: 1)
-
+    @user_tasks = Task.where(user_id: current_user.id)
+    @chart = Chart.new
   end
 
   def new
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    flash[:notice] = "Thank you for creating a task!"
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :task_type, :user_id, :times_used)
+
+  end
 end
